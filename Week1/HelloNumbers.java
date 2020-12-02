e0ffedd6f7161787dfe6dd4ec32eb8b0cbae59e5
Sreekaran <sreekaran.srinath@gmail.com>
2020-12-02 10:22:54 +0530

    Add HelloNumbers.java

---
+++ Week1/HelloNumbers.java
@@ -0,0 +1,11 @@
+package Week1;
+
+public class HelloNumbers {
+    public static void main(String[] args){
+        int x = 0;
+        while (x < 10){
+            System.out.print(x + " ");
+            x += 1;
+        }
+    }
+}
---
+++ Week1/HelloWorld.java
@@ -0,0 +1,7 @@
+package Week1;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("This is technically not my hello world in Java, but I'd like to start on a clean, fresh slate, soo... Hello World!");
+    }
+}
